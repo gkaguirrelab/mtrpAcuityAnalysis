ff2dace339e1ecfd0e6829d5dd57e24263ce6cf8
gkaguirre <gkaguirre@me.com>
2019-07-26 14:59:07 -0400

    Extend demo across subjects
    Signed-off-by: gkaguirre <gkaguirre@me.com>

--- code/demoPlots.m
+++ code/demoPlots.m
@@ -1,20 +1,47 @@
 
-load('../data/Subject_AOSO_11096_axisAcuityData.mat');
+subjectIDs = {'11060','11089','11096'};
 
+% Identify the data location and set up the stimulus properties
+dataBasePath = getpref('mtrpAcuityAnalysis','mtrpCompiledDataPath');
+titleSets = {'2.5 degrees','5 degrees','10 degrees','20 degrees'};
+eccenVals = [2.5,5,10,20];
 positionSets = {...
     {[-2.5, 0],[2.5,0],[0,2.5],[0,-2.5],[1.77,1.77],[-1.77,-1.77],[-1.77,1.77],[1.77,-1.77]};...
-     {[-5, 0],[5,0],[0,5],[0,-5],[3.54,3.54],[-3.54,-3.54],[-3.54,3.54],[3.54,-3.54]};...
+    {[-5, 0],[5,0],[0,5],[0,-5],[3.54,3.54],[-3.54,-3.54],[-3.54,3.54],[3.54,-3.54]};...
     {[-10, 0],[10,0],[0,10],[0,-10],[7.07,7.07],[-7.07,-7.07],[-7.07,7.07],[7.07,-7.07]};...
     {[-20, 0],[20,0],[0,20],[0,-20],[14.14,14.14],[-14.14,-14.14],[-14.14,14.14],[14.14,-14.14]};...
     };
 
-titleSets = {'2.5 degrees','5 degrees','10 degrees','20 degrees'};
+% Set up a figure to hold the threshold results
+threshFigHandle = figure('NumberTitle', 'off', 'Name', 'Threshold values across subjects');
 
-for ii=1:length(positionSets)
-    figure('NumberTitle', 'off', 'Name', titleSets{ii});
-    subplot(2,1,1);
-    plotStaircase(axisAcuityData, positionSets{ii});
-    subplot(2,1,2);
-    plotPercentCorrectByBin(axisAcuityData, positionSets{ii});
+% Loop over the subjects
+for ss=1:length(subjectIDs)
+    
+    % Load the data for this subject
+    dataFileName = fullfile(dataBasePath,['Subject_AOSO_',subjectIDs{ss},'_axisAcuityData.mat']);
+    load(dataFileName,'axisAcuityData')
+    
+    % Loop over the sets of eccentricity positions
+    threshVals = zeros(1,length(positionSets));
+    for ii=1:length(positionSets)
+        figure('NumberTitle', 'off', 'Name', [subjectID ' - ' titleSets{ii}]);
+        subplot(2,1,1);
+        plotStaircase(axisAcuityData, positionSets{ii});
+        subplot(2,1,2);
+        threshVals(ii) = plotPercentCorrectByBin(axisAcuityData, positionSets{ii});
+    end
+    
+    % Plot this set of thresh vals
+    figure(threshFigHandle);
+    plot(eccenVals,threshVals,'o-');
+    hold on
 end
 
+% Finish the figure
+figure(threshFigHandle);
+xlim([0 25]);
+ylim([1 20]);
+xlabel('Eccentricity [deg]');
+ylabel('Stimulus threshold for 50% performance [cycles/deg]');
+legend(subjectIDs)
